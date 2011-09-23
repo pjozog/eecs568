402308d897e943e906a1a4411319166d725851fe
Schuyler Cohen <cohensh@gmail.com>
2011-09-23 13:09:47 -0400

    Added problem 3, with contours and points

---
+++ java/src/team/Radar.java
@@ -0,0 +1,159 @@
+package team;
+
+import javax.swing.*; // For JFrame
+import java.awt.*; // For BorderLayout
+import java.util.*;
+
+import april.vis.*; // For VisCanvas etc
+import april.util.*; // For Parameter GUI
+import april.jmat.*;
+
+
+
+public class Radar
+{
+    public static void main(String []args){
+
+	JFrame jf = new JFrame("VisDemo");
+        jf.setSize(640,480);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final VisWorld vw = new VisWorld();
+        VisLayer vl = new VisLayer(vw);
+        VisCanvas vc = new VisCanvas(vl);
+
+	ParameterGUI pg = new ParameterGUI();
+
+      
+        jf.setLayout(new BorderLayout());
+        jf.add(vc,BorderLayout.CENTER);
+        jf.add(pg,BorderLayout.SOUTH);
+
+
+        jf.setVisible(true);
+	
+	double rMean = 100;
+	double thetaMean = 0;
+	
+	double mean [] = new double[]{rMean, thetaMean};
+	double cov[][] = new double[2][2];
+
+	//Starting with r, ending with theta, assuming independent
+
+	cov[0][0] = 50;
+	cov[0][1] = 0;
+	cov[1][0] = 0;
+	cov[1][1] = 0.15;
+
+	MultiGaussian gauss = new MultiGaussian(cov, mean);
+
+	int numObservations = 500;
+
+
+	ArrayList<double[]> points = new ArrayList<double[]>();
+
+	Random rand = new Random();
+	for(int i = 0; i < numObservations; i++){
+
+	    double sample [] = gauss.sample(rand);
+
+	    if(sample.length != 2){
+		System.out.println("Expecting two values, got " + sample.length);
+	    }
+
+	    //0 is r, 1 is theta
+	    double r = sample[0];
+	    double theta = sample[1];
+       
+	    double x = r * Math.cos(theta);
+	    double y = r * Math.sin(theta);
+	    double pt[] = new double[]{x, y};
+	    points.add(pt);
+
+	}
+	int sigma = 3;
+	int chi2 = sigma * sigma;
+
+
+	MultiGaussian gaussXY = new MultiGaussian(points);
+	ArrayList<double[]> contour = gaussXY.getContour(chi2);
+	
+
+	points.addAll(contour);
+
+	double [][]jac = new double[2][2];
+	
+	jac[0][0] = Math.cos(thetaMean);
+	jac[1][0] = Math.sin(thetaMean);
+	jac[0][1] = -rMean * Math.sin(thetaMean);
+	jac[1][1] = rMean * Math.cos(thetaMean);
+
+	double [][] jacT = LinAlg.copy(LinAlg.transpose(jac));
+	System.out.println("jac");
+	ArrayUtil.print2dArray(jac);
+	System.out.println("jact");
+	ArrayUtil.print2dArray(jacT);
+
+	double [][] projCovXY = LinAlg.copy(LinAlg.matrixAB(jac, LinAlg.matrixAB(cov, jacT)));
+
+	double [] projMeanXY = LinAlg.copy(LinAlg.matrixAB(jac, gauss.getMean()));
+
+	MultiGaussian projGauss = new MultiGaussian(projCovXY, projMeanXY);
+
+	ArrayUtil.print2dArray(projCovXY);
+	ArrayUtil.print1dArray(projMeanXY);
+	
+
+	ArrayList<double[]> projContour = projGauss.getContour(chi2);
+
+	points.addAll(projContour);
+
+
+		
+	VisVertexData vd = new VisVertexData(points);
+
+	VisColorData vcd = new VisColorData();
+
+	for(int i = 0 ; i < numObservations; i++){
+	    int col = 0xff0000ff;
+	    vcd.add(col);
+	}
+	for(int i = 0; i < contour.size(); i++){
+	    int col = 0xffff0000;
+	    vcd.add(col);
+	    
+	}
+	for(int i = 0; i < projContour.size(); i++){
+	    int col = 0xff00ffff;
+	    vcd.add(col);
+	}
+
+
+
+
+	
+	{
+	    VisWorld.Buffer vb = vw.getBuffer("cloud");
+	    vb.addBack(new VisPoints(vd, vcd, 4));
+	    vb.swap();
+	}
+        // Drawing code
+        {
+            VisGrid vg = new VisGrid(new Color(.5f,.5f,.5f),
+                                     new Color(1.0f,1.0f,1.0f,0.0f));
+	    
+            VisWorld.Buffer vb = vw.getBuffer("grid");
+            vb.setDrawOrder(-100);
+            vb.addBack(new VisDepthTest(false,vg));
+            vb.swap();
+        }
+
+
+	
+	
+
+
+
+    }
+
+}

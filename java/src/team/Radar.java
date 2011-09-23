38c14b3f4a5ae162672165c706fad323eb7c9577
Paul Ozog <devnull@localhost>
2011-09-23 14:06:16 -0400

    Added sliders for Task 3 (Radar problem)

--- java/src/team/Radar.java
+++ java/src/team/Radar.java
@@ -23,7 +23,11 @@ public class Radar
         VisCanvas vc = new VisCanvas(vl);
 
 	ParameterGUI pg = new ParameterGUI();
-
+        pg.addDoubleSlider("sigRange", "Range Sigma", 0.0001, 100, 50.0);
+        pg.addDoubleSlider("sigTheta", "Theta Sigma", 0.0001, 0.5, 0.15);
+        pg.addDoubleSlider("sigCross","Range/Theta Covariance",    -10, 10, 0.0);
+	pg.addDoubleSlider("meanRange", "Range Mean (m)",   0, 1000, 100);
+	pg.addDoubleSlider("meanTheta", "Theta Mean (degrees)",   -180, 180, 0);
       
         jf.setLayout(new BorderLayout());
         jf.add(vc,BorderLayout.CENTER);
@@ -32,127 +36,109 @@ public class Radar
 
         jf.setVisible(true);
 	
-	double rMean = 100;
-	double thetaMean = 0;
+        pg.addListener(new ParameterListener() {
+	    public void parameterChanged(ParameterGUI pg, String name)
+	    { 
+		   
+		if (name.equals("sigRange") || 
+		    name.equals("sigTheta") ||
+		    name.equals("sigCross") ||  
+		    name.equals("meanRange") ||
+		    name.equals("meanTheta")) {
+
+		    double rMean     = 100;
+		    double thetaMean = 0;
 	
-	double mean [] = new double[]{rMean, thetaMean};
-	double cov[][] = new double[2][2];
+		    //Starting with r, ending with theta
+		    double mean []   = new double[]{pg.gd("meanRange"), pg.gd("meanTheta")};
+		    double cov[][]   = new double[2][2];
 
-	//Starting with r, ending with theta, assuming independent
+		    cov[0][0] = pg.gd("sigRange");
+		    cov[0][1] = pg.gd("sigCross");
+		    cov[1][0] = pg.gd("sigCross");
+		    cov[1][1] = pg.gd("sigTheta");
 
-	cov[0][0] = 50;
-	cov[0][1] = 0;
-	cov[1][0] = 0;
-	cov[1][1] = 0.15;
+		    MultiGaussian gauss = new MultiGaussian(cov, mean);
 
-	MultiGaussian gauss = new MultiGaussian(cov, mean);
+		    int numObservations = 500;
 
-	int numObservations = 500;
+		    ArrayList<double[]> points = new ArrayList<double[]>();
 
+		    Random rand = new Random();
+		    for(int i = 0; i < numObservations; i++){
 
-	ArrayList<double[]> points = new ArrayList<double[]>();
+			double sample [] = gauss.sample(rand);
 
-	Random rand = new Random();
-	for(int i = 0; i < numObservations; i++){
+			if(sample.length != 2){
+			    System.out.println("Expecting two values, got " + sample.length);
+			}
 
-	    double sample [] = gauss.sample(rand);
-
-	    if(sample.length != 2){
-		System.out.println("Expecting two values, got " + sample.length);
-	    }
-
-	    //0 is r, 1 is theta
-	    double r = sample[0];
-	    double theta = sample[1];
+			//0 is r, 1 is theta
+			double r = sample[0];
+			double theta = sample[1];
        
-	    double x = r * Math.cos(theta);
-	    double y = r * Math.sin(theta);
-	    double pt[] = new double[]{x, y};
-	    points.add(pt);
+			double x = r * Math.cos(theta);
+			double y = r * Math.sin(theta);
+			double pt[] = new double[]{x, y};
+			points.add(pt);
 
-	}
-	int sigma = 3;
-	int chi2 = sigma * sigma;
+		    }
 
+		    int sigma                   = 3;
+		    int chi2                    = sigma * sigma;
+		    MultiGaussian gaussXY       = new MultiGaussian(points);
+		    ArrayList<double[]> contour = gaussXY.getContour(chi2);
 
-	MultiGaussian gaussXY = new MultiGaussian(points);
-	ArrayList<double[]> contour = gaussXY.getContour(chi2);
+		    points.addAll(contour);
+
+		    double [][]jac = new double[2][2];
 	
+		    jac[0][0] = Math.cos(thetaMean);
+		    jac[1][0] = Math.sin(thetaMean);
+		    jac[0][1] = -rMean * Math.sin(thetaMean);
+		    jac[1][1] = rMean * Math.cos(thetaMean);
 
-	points.addAll(contour);
+		    double [][] jacT        = LinAlg.copy(LinAlg.transpose(jac));
+		    double [][] projCovXY   = LinAlg.copy(LinAlg.matrixAB(jac, LinAlg.matrixAB(cov, jacT)));
+		    double [] projMeanXY    = LinAlg.copy(LinAlg.matrixAB(jac, gauss.getMean()));
+		    MultiGaussian projGauss = new MultiGaussian(projCovXY, projMeanXY);
 
-	double [][]jac = new double[2][2];
-	
-	jac[0][0] = Math.cos(thetaMean);
-	jac[1][0] = Math.sin(thetaMean);
-	jac[0][1] = -rMean * Math.sin(thetaMean);
-	jac[1][1] = rMean * Math.cos(thetaMean);
+		    ArrayList<double[]> projContour = projGauss.getContour(chi2);
 
-	double [][] jacT = LinAlg.copy(LinAlg.transpose(jac));
-	System.out.println("jac");
-	ArrayUtil.print2dArray(jac);
-	System.out.println("jact");
-	ArrayUtil.print2dArray(jacT);
-
-	double [][] projCovXY = LinAlg.copy(LinAlg.matrixAB(jac, LinAlg.matrixAB(cov, jacT)));
-
-	double [] projMeanXY = LinAlg.copy(LinAlg.matrixAB(jac, gauss.getMean()));
-
-	MultiGaussian projGauss = new MultiGaussian(projCovXY, projMeanXY);
-
-	ArrayUtil.print2dArray(projCovXY);
-	ArrayUtil.print1dArray(projMeanXY);
-	
-
-	ArrayList<double[]> projContour = projGauss.getContour(chi2);
-
-	points.addAll(projContour);
-
-
+		    points.addAll(projContour);
 		
-	VisVertexData vd = new VisVertexData(points);
+		    VisVertexData vd = new VisVertexData(points);
+		    VisColorData vcd = new VisColorData();
 
-	VisColorData vcd = new VisColorData();
+		    for(int i = 0 ; i < numObservations; i++){
+			int col = 0xff0000ff;
+			vcd.add(col);
+		    }
+		    for(int i = 0; i < contour.size(); i++){
+			int col = 0xffff0000;
+			vcd.add(col);
+		    }
+		    for(int i = 0; i < projContour.size(); i++){
+			int col = 0xff00ffff;
+			vcd.add(col);
+		    }
 
-	for(int i = 0 ; i < numObservations; i++){
-	    int col = 0xff0000ff;
-	    vcd.add(col);
-	}
-	for(int i = 0; i < contour.size(); i++){
-	    int col = 0xffff0000;
-	    vcd.add(col);
+		    VisWorld.Buffer vb = vw.getBuffer("cloud");
+		    vb.addBack(new VisPoints(vd, vcd, 4));
+		    vb.swap();
+
+		}
+	    }
+	});
+
+	// Drawing code
+	VisGrid vg = new VisGrid(new Color(.5f,.5f,.5f),
+				 new Color(1.0f,1.0f,1.0f,0.0f));
 	    
-	}
-	for(int i = 0; i < projContour.size(); i++){
-	    int col = 0xff00ffff;
-	    vcd.add(col);
-	}
-
-
-
-
-	
-	{
-	    VisWorld.Buffer vb = vw.getBuffer("cloud");
-	    vb.addBack(new VisPoints(vd, vcd, 4));
-	    vb.swap();
-	}
-        // Drawing code
-        {
-            VisGrid vg = new VisGrid(new Color(.5f,.5f,.5f),
-                                     new Color(1.0f,1.0f,1.0f,0.0f));
-	    
-            VisWorld.Buffer vb = vw.getBuffer("grid");
-            vb.setDrawOrder(-100);
-            vb.addBack(new VisDepthTest(false,vg));
-            vb.swap();
-        }
-
-
-	
-	
-
-
+	VisWorld.Buffer vb = vw.getBuffer("grid");
+	vb.setDrawOrder(-100);
+	vb.addBack(new VisDepthTest(false,vg));
+	vb.swap();
 
     }
 

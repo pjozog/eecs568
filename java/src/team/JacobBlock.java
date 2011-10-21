87913d4f147fecbb91be70f6f69ff920d9d37b15
Pat O'Keefe <patokeefe1@gmail.com>
2011-10-21 16:24:06 -0400

    Fixed merge error

--- java/src/team/JacobBlock.java
+++ java/src/team/JacobBlock.java
@@ -1,7 +1,6 @@
 package team;
 
 import april.jmat.*;
-import java.util.*;
 
 public class JacobBlock{
 
@@ -12,39 +11,57 @@ public class JacobBlock{
     private int block1Col;
     private int block2Col;
 
+
+    public JacobBlock(int rowStart, int blockOneCol, int blockTwoCol) {
+
+        this.row = rowStart;
+        this.block1Col = blockOneCol;
+        this.block2Col = blockTwoCol;
+
+    }
+
     /** rows is the size of the observation vector, columns is the size of the state vector**/
     public static Matrix assemble(int rows, int cols, ArrayList<JacobBlock> blocks){
 	
-	Matrix toReturn = new Matrix(rows, cols, Matrix.SPARSE);
+        Matrix toReturn = new Matrix(rows, cols, Matrix.SPARSE);
 	
-	for(JacobBlock block : blocks){
+        for(JacobBlock block : blocks){
 	    
-	    toReturn.set(block.getRow(), block.getFirstColumn(),  block.getFirstBlock());
-	    toReturn.set(block.getRow(), block.getSecondColumn(), block.getSecondBlock());
+            toReturn.set(block.getRow(), block.getFirstColumn(),  block.getFirstBlock());
+            toReturn.set(block.getRow(), block.getSecondColumn(), block.getSecondBlock());
 	
-	}
+        }
 
-	return toReturn;
+        return toReturn;
     }
 
+
     public int getRow(){
-	return row;
+        return row;
     }
 
     public int getFirstColumn(){
-	return block1Col;
+        return block1Col;
     }
 
-    public int getSecondColumn(){
-	return block2Col;
+    public int getSecondColum(){
+        return block2Col;
     }
 
     public double[][] getFirstBlock(){
-	return block1;
+        return block1;
     }
 
     public double[][] getSecondBlock(){
-	return block2;
+        return block2;
+    }
+
+    public void setFirstBlock(double[][] theBlock) {
+        this.block1 = theBlock;
+    }
+
+    public void setSecondBlock(double[][] theBlock) {
+        this.block2 = theBlock;
     }
 
     

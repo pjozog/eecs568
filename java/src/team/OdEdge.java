1628fe53189810ae70cf0e27aa21021112d505a3
Pat O'Keefe <patokeefe1@gmail.com>
2011-10-21 16:45:17 -0400

    Finished jacobian getter for OdEdge.

--- java/src/team/OdEdge.java
+++ java/src/team/OdEdge.java
@@ -2,4 +2,67 @@ package team;
 
 public class OdEdge extends Edge{
 
+    private int jacobianStartRow;
+    private int block1Column;
+    private int block2Column;
+
+
+    public OdEdge(int jacobStartRow, int firstBlockColumnStart, int secondBlockColumnStart) {
+        
+        this.jacobianStartRow = jacobStartRow;
+        this.block1Column = firstBlockColumnStart;
+        this.block2Column = secondBlockColumnStart;
+        
+    }
+
+
+    public JacobBlock getJacob(double[] theStateVector){
+	
+        // Create a new JacobBlock class
+        JacobBlock myJacobBlock = new JacobBlock(jacobStartRow, block1Column, block2Column);
+
+        double x0  = theStateVector[this.node1.getStateVectorIndex()];
+        double y0  = theStateVector[this.node1.getStateVectorIndex()+1]; 
+        double phi0 = theStateVector[this.node1.getStateVectorIndex()+2];
+        double x1 = theStateVector[this.node2.getStateVectorIndex()];                             
+        double y1 = theStateVector[this.node2.getStateVectorIndex()+1];
+                                     
+        // Jacobian with respect to a position
+        // Create block one ... should be 3x3
+        double[][] firstBlock = new double[3][3];
+        firstBlock[0][0] = -Math.cos(phi0);
+		firstBlock[0][1] = -Math.sin(phi0);
+		firstBlock[0][2] = Math.sin(phi0)*(x0-x1) - Math.cos(phi0)*(y0-y1);
+		firstBlock[1][0] = Math.sin(phi0);
+		firstBlock[1][1] = -Math.cos(phi0);
+		firstBlock[1][2] = Math.cos(phi0)*(x0 - x1) + Math.sin(phi0)*(y0 - y1);
+		firstBlock[2][0] = 0.0;
+		firstBlock[2][1] = 0.0;
+		firstBlock[2][2] = -1.0
+
+        myJacobBlock.setFirstBlock(firstBlock);
+
+
+        // Jacobian with respect to a position
+        // Create block two  ... should be 3x3
+        double[][] secondBlock = new double[3][3];         
+        secondBlock[0][0] = Math.cos(phi0);
+		secondBlock[0][1] = Math.sin(phi0);
+		secondBlock[0][2] = 0.0;
+		secondBlock[1][0] = -Math.sin(phi0);
+		secondBlock[1][1] = Math.cos(phi0);
+		secondBlock[1][2] = 0.0;
+		secondBlock[2][0] = 0.0;
+		secondBlock[2][1] = 0.0;
+        secondBlock[2][2] = 1.0;
+
+
+        myJacobBlock.setSecondBlock(setSecondBlock);
+
+        return myJacobBlock;
+
+    }
+
+
+
 }

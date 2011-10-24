8cc1db2f54a7d541ca6206aee4af547028ad05e3
Paul Ozog <devnull@localhost>
2011-10-23 20:40:56 -0400

    Added some BS results

--- java/src/team/plotSurfaceErrors.py
+++ java/src/team/plotSurfaceErrors.py
@@ -14,8 +14,22 @@ landNoise = array([1,2,4,8])
 X, Y = np.meshgrid(odomNoise, landNoise)
 
 Z = zeros((odomNoise.size, landNoise.size))
-Z[0,2] = .5
-Z[1,3] = .9
+Z[ 0, 0] =  68
+Z[ 0, 1] =  62
+Z[ 0, 2] =  77
+Z[ 0, 3] =  65
+Z[ 1, 0] =  44
+Z[ 1, 1] =  72
+Z[ 1, 2] =  73
+Z[ 1, 3] =  52
+Z[ 2, 0] =  72
+Z[ 2, 1] =  2 
+Z[ 2, 2] =  58
+Z[ 2, 3] =  52
+Z[ 3, 0] =  27
+Z[ 3, 1] =  78
+Z[ 3, 2] =  63
+Z[ 3, 3] =  66
 
 
 surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
@@ -27,14 +41,16 @@ surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.jet,
 # cset = ax.contour(X, Y, Z, zdir='y', offset=1, alpha=0.5)
 
 # Set our labels
-ax.set_xlabel('Odometry Noise')
-ax.set_ylabel('Landmark Sensor Noise')
+ax.set_xlabel('Normalized Landmark Sensor Noise')
+ax.set_ylabel('Normalized Odometry Noise')
 ax.set_zlabel('Number of Errors')
 
 ax.set_title("Number of Data Association Errors versus Noise")
 
 # This seems nice
-# ax.view_init(elev=25, azim=45)
+ax.view_init(elev=54, azim=37)
 
-# savefig("%sFitness3D.png" % (imagePath), transparent=False)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+savefig("partB.png")
 

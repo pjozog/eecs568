c4286837a9b48314a334e9bcfab76917806f3295
Paul Ozog <devnull@localhost>
2011-11-04 11:38:09 -0400

    Added jacobians to motion model class

--- java/src/team/FastSLAMMotionModel.java
+++ java/src/team/FastSLAMMotionModel.java
@@ -1,4 +1,4 @@
-package april.jmat;
+import april.jmat.*;
 import java.lang.Math;
 
 class FastSLAMMotionModel {
@@ -38,6 +38,49 @@ class FastSLAMMotionModel {
 
     }
 
+    public static double[][] jacobianRw(double[] posXYT, double[] rTheta) {
+
+	//index by jacob[rol][col]
+	double[][] jacob = new double[2][2];
+
+	double r     = rTheta[0];
+	double theta = rTheta[1];
+	double poseT = posXYT[2];
+
+
+	jacob[0][0] =    Math.cos(poseT + theta); 
+	jacob[0][1] = -r*Math.cos(poseT + theta);
+
+	jacob[1][0] =    Math.sin(poseT + theta); 
+	jacob[1][1] =  r*Math.cos(poseT + theta);
+
+	return jacob;
+
+    }
+
+    public static double[][] jacobianJx(double[] posXYT, double[] featPos) {
+
+	double l_x = featPos[0];
+	double l_y = featPos[1];
+	double x0  = posXYT[0];
+	double y0  = posXYT[1];
+
+	//index by jacob[rol][col]
+        double[][] jacob = new double[2][2];
+        jacob[0][0]      =   (2*l_x - 2*x0)/(2*Math.sqrt(Math.pow(l_x - x0,2) + Math.pow(l_y - y0,2)));
+        jacob[0][1]      =   (2*l_y - 2*y0)/(2*Math.sqrt(Math.pow(l_x - x0,2) + Math.pow(l_y - y0,2)));
+        jacob[1][0]      =   -(l_y - y0)/(Math.pow(l_x - x0,2)*(Math.pow(l_y - y0,2)/Math.pow(l_x - x0,2) + 1));
+        jacob[1][1]      =   1/((l_x - x0)*(Math.pow(l_y - y0,2)/Math.pow(l_x - x0,2) + 1));
+
+	return jacob;
+
+    }
+
+    public static double[][] jacobianJw() {
+	
+	return Matrix.identity(2, 2).copyArray();
+
+    }
 
 
 }

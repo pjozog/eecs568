0429ee56e7eb1b0e725c8c2852afe3ef3b4272ee
Pat O'Keefe <patokeefe1@gmail.com>
2011-11-22 16:43:37 -0500

    Further stubbage. 5 percent complete.

--- java/src/team/sim/Listener.java
+++ java/src/team/sim/Listener.java
@@ -25,6 +25,14 @@ public class Listener implements OldSimulator.Listener {
         vw = _vw;
     }
 
+    /**
+     * Adds a Pose3DNode and a Pose3DEdge to the graph. This is needed so we don't have an
+     * underconstrained systen.
+     */
+    public void addPrior() {
+
+    }
+
     public void update(OldSimulator.odometry_t odom, ArrayList<OldSimulator.landmark_t> dets) {
 
         drawDummy(dets);
--- java/src/team/slam/BackEnd.java
+++ java/src/team/slam/BackEnd.java
@@ -1,7 +1,67 @@
 package team.slam;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class BackEnd{
-    
+
+    private List<Node> nodes;
+    private List<Edge> edges;
 
 
-} 
+    // Book-keeping
+    private int nodeDimension;
+    private int edgeDimension;
+    private int numNewRows;
+    private int numNewMeasurements;
+
+    /**
+     * Default constructor. Just ivar initializations.
+     */
+    public BackEnd() {
+
+        nodes = new ArrayList<Node>();
+        edges = new ArrayList<Edge>();
+
+        nodeDimension      = 0;
+        edgeDimension      = 0;
+        numNewRows         = 0;
+        numNewMeasurements = 0;
+
+
+
+    }
+
+    /**
+     * Add the node to our list. This assumes that the front end will only call this
+     * function if the node is not already a part of the graph.
+     */
+    public void addNode(Node node) {
+
+        nodes.add(node);
+
+        nodeDimension += node.getDOF();
+
+    }
+
+    /**
+     * Add the edge to our list. Book-keeping and initialization?
+     */
+    public void addEdge(Edge edge) {
+
+    }
+
+    /**
+     * Find the least sqaures solution of the system. This will construct the Jacabian and
+     * evaluate it at the best state vector estimate, assemble JtSigmaJ, compute the
+     * residual, and find the new optimal stuff.
+     */
+    public boolean solve() {
+
+    }
+
+    //TODO: Accessors to get solution. Depends on how we want to draw things.
+
+
+
+}

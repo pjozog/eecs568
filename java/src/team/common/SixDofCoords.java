b90b5a01f541e7b3ae8d3e67bf6115ddb4e35a23
Paul Ozog <devnull@localhost>
2011-11-27 19:34:36 -0500

    Using symoblic jacobians (which makes a 10 second difference) and making waittime 0 (it makes a 5 second difference)

--- java/src/team/common/SixDofCoords.java
+++ java/src/team/common/SixDofCoords.java
@@ -209,79 +209,62 @@ public class SixDofCoords {
     }
 
     /**
-     * Numerically compute jacobian for headToTail
-     */
-    public static double[][] headToTailJacob(double[] xij, double[] xjk) {
-
-        double eps = 1e-6;
+     * Compute jacobian for headToTail (see Eustice pg 150)
+     */ 
+   public static double[][] headToTailJacob(double[] xij, double[] xjk) {
 
         Matrix J = new Matrix(6, 12);
 
-        //Blacklisted elements
-        /*
-          I M | R 0
-          0 K | 0 K
-         */
+        double[] xik = headToTail(xij, xjk);
 
-        //Differentiate with respect to xij
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        Matrix Hij = new Matrix(LinAlg.xyzrpyToMatrix(xij));
+        Matrix _Rji = new Matrix(Hij.copyArray(0, 0, 3, 3));
+        Matrix _Rij = _Rji.transpose();
+        double[][] Rji = _Rji.copyArray();
+        double[][] Rij = _Rij.copyArray();
 
-                if (i == 0 || i == 1 || i == 2) {
-                    if (j == 0 || j == 1 | j == 2) {
-                        if (j == i) {
-                            J.set(j, i, 1.0);
-                        } else {
-                            J.set(j, i, 0.0);
-                        }
-                    } else {
-                        J.set(j, i, 0.0);
-                    }
-                } else {
+        Matrix Hjk = new Matrix(LinAlg.xyzrpyToMatrix(xjk));
+        Matrix _Rkj = new Matrix(Hjk.copyArray(0, 0, 3, 3));
+        Matrix _Rjk = _Rkj.transpose();
+        double[][] Rkj = _Rkj.copyArray();
+        double[][] Rjk = _Rjk.copyArray();
 
-                    double[] xijPert = new double[xij.length];
-                    for (int k = 0; k < xij.length; k++)
-                        xijPert[k] = xij[k];
+        double R13 = Rji[0][2]; double R12 = Rji[0][1];
+        double R23 = Rji[1][2]; double R22 = Rji[1][1];
+        double R33 = Rji[2][2]; double R32 = Rji[2][1];
 
-                    xijPert[i] += eps;
+        double Rkj12 = Rkj[0][1]; double Rkj13 = Rkj[0][2];
+        
+        double Rij13 = Rij[0][2]; double Rij23 = Rij[1][2];
 
-                    double[] y = headToTail(xij, xjk);
-                    double[] yPert = headToTail(xijPert, xjk);
+        double x_ij = xij[0]; double y_ij = xij[1]; double z_ij = xij[2];
+        double x_jk = xjk[0]; double y_jk = xjk[1]; double z_jk = xjk[2];
+        double x_ik = xik[0]; double y_ik = xik[1]; double z_ik = xik[2];
 
-                    double finiteDiff = (yPert[j] - y[j]) / eps;
+        double phi_ij = xij[3]; double theta_ij = xij[4]; double psi_ij = xij[5];
+        double phi_jk = xjk[3]; double theta_jk = xjk[4]; double psi_jk = xjk[5];
+        double phi_ik = xik[3]; double theta_ik = xik[4]; double psi_ik = xik[5];
 
-                    J.set(j, i, finiteDiff);
-                }
-            }
-        }
+        Matrix I = Matrix.identity(3,3);
+        Matrix O = new Matrix(3,3);
 
-        //Now for xjk
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        double[][] M = new double[3][3];
+        M[0][0] = R13*y_jk - R12*z_jk; M[0][1] = (z_ik - z_ij)*cos(psi_ij); M[0][2] = -(y_ik - y_ij);
+        M[1][0] = R23*y_jk - R22*z_jk; M[1][1] = (z_ik - z_ij)*sin(psi_ij); M[1][2] = (x_ik - x_ij);
+        M[2][0] = R33*y_jk - R32*z_jk; M[2][1] = -x_jk*cos(theta_ij) - (y_jk * sin(phi_ij) + z_jk*cos(phi_ij))*sin(theta_ij); M[2][2] = 0;
 
-                if ((i == 0 || i == 1 || i == 2) &&
-                    (j == 4 || j == 5 || j == 6)) {
-                    J.set(j, i+6, 0.0);
-                } else if ((i == 4 || i == 5 || i == 6) &&
-                           (j == 0 || j == 1 || j == 2)) {
-                    J.set(j, i+6, 0.0);                    
-                } else {
+        double[][] K1 = new double[3][3];
+        K1[0][0] = cos(theta_ij)*cos(psi_ik - psi_ij)*sec(theta_ik); K1[0][1] = sin(psi_ik - psi_ij)*sec(theta_ik); K1[0][2] = 0;
+        K1[1][0] = -cos(theta_ij)*sin(psi_ik - psi_ij); K1[1][1] = cos(psi_ik - psi_ij); K1[1][2] = 0;
+        K1[2][0] = Rkj12*sin(phi_ik) + Rkj13*cos(phi_ik)*sec(theta_ik); K1[2][1] = sin(psi_ik - psi_ij)*tan(theta_ik); K1[2][2] = 1;
 
-                    double[] xjkPert = new double[xjk.length];
-                    for (int k = 0; k < xjk.length; k++)
-                        xjkPert[k] = xjk[k];
+        double[][] K2 = new double[3][3];
+        K2[0][0] = 1; K2[0][1] = sin(phi_ik - phi_jk)*tan(theta_ik); K2[0][2] = (R13*cos(psi_ik) + R23*sin(psi_ik))*sec(theta_ik);
+        K2[1][0] = 0; K2[1][1] = cos(phi_ik - phi_jk); K2[1][2] = -cos(theta_jk)*sin(phi_ik - phi_jk);
+        K2[2][0] = 0; K2[2][1] = sin(phi_ik - phi_jk)*sec(theta_ik); K2[2][2] = cos(theta_jk)*cos(phi_ik - phi_jk)*sec(theta_ik);
 
-                    xjkPert[i] += eps;
-
-                    double[] y = headToTail(xij, xjk);
-                    double[] yPert = headToTail(xij, xjkPert);
-
-                    double finiteDiff = (yPert[j] - y[j]) / eps;
-
-                    J.set(j, i+6, finiteDiff);
-                }
-            }
-        }
+        J.set(0, 0, I.copyArray()); J.set(0, 3, M); J.set(0, 6, Rji); J.set(0, 9, O.copyArray());
+        J.set(3, 0, O.copyArray()); J.set(3, 3, K1); J.set(3,6, O.copyArray()); J.set(3, 9, K2);
 
         return J.copyArray();
 
@@ -316,37 +299,71 @@ public class SixDofCoords {
      */
     public static double[][] inverseJacob(double[] xij) {
 
-        double eps = 1e-6;
-
         Matrix J = new Matrix(6, 6);
 
-        //Differentiate with respect to xij
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        double[] xji = inverse(xij);
 
-                if ((i == 0 || i == 1 || i == 2) &&
-                    (j == 3 || j == 4 || j == 5)) {
-                    J.set(i, j, 0.0);
-                } else {
+        Matrix Hij = new Matrix(LinAlg.xyzrpyToMatrix(xij));
+        Matrix _Rji = new Matrix(Hij.copyArray(0, 0, 3, 3));
+        Matrix _Rij = _Rji.transpose();
+        double[][] Rji = _Rji.copyArray();
+        double[][] Rij = _Rij.copyArray();
 
-                    double[] xijPert = new double[xij.length];
-                    for (int k = 0; k < xij.length; k++)
-                        xijPert[k] = xij[k];
+        double R13 = Rji[0][2]; double R12 = Rji[0][1]; double R11 = Rji[0][0];
+        double R23 = Rji[1][2]; double R22 = Rji[1][1]; double R21 = Rji[1][0];
+        double R33 = Rji[2][2]; double R32 = Rji[2][1]; double R31 = Rji[2][0];
 
-                    xijPert[i] += eps;
+        double x_ij = xij[0]; double y_ij = xij[1]; double z_ij = xij[2];
+        double x_ji = xji[0]; double y_ji = xji[1]; double z_ji = xji[2];
 
-                    double[] y = inverse(xij);
-                    double[] yPert = inverse(xijPert);
+        double phi_ij = xij[3]; double theta_ij = xij[4]; double psi_ij = xij[5];
+        double phi_ji = xji[3]; double theta_ji = xji[4]; double psi_ji = xji[5];
 
-                    double finiteDiff = (yPert[j] - y[j]) / eps;
+        double[][] N = new double[3][3];
+        N[0][0] = 0;     N[0][1] = -R31*(x_ij*cos(psi_ij) + y_ij*sin(psi_ij)) + z_ij*cos(theta_ij);             N[0][2] = R21*x_ij - R11*y_ij;
+        N[1][0] = z_ji;  N[1][1] = -R32*(x_ij*cos(psi_ij) + y_ij*sin(psi_ij)) + z_ij*sin(theta_ij)*sin(phi_ij); N[1][2] = R22*x_ij - R12*y_ij;
+        N[2][0] = -y_ji; N[2][1] = -R33*(x_ij*cos(psi_ij) + y_ij*sin(psi_ij)) + z_ij*sin(theta_ij)*cos(phi_ij); N[2][2] = R23*x_ij - R13*y_ij;
 
-                    J.set(j, i, finiteDiff);
-                }
-            }
-        }
+        double alpha = 1 / (1 - sq(R13));
+        double[][] Q = new double[3][3];
+        Q[0][0] = -R11; Q[0][1] = -R12*cos(phi_ij); Q[0][2] = R13*R33;
+        Q[1][0] = R12*sqrt(1-sq(R13)); Q[1][1] = -R33*cos(psi_ij)*sqrt(1 - sq(R13)); Q[1][2] = R23 * sqrt(1-sq(R13));
+        Q[2][0] = R13*R11; Q[2][1] = -R23*cos(psi_ij); Q[2][2] = -R33;
+
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
+                Q[row][col] *= alpha;
+
+        J.set(0, 0, _Rji.transpose().times(-1.0).copyArray());
+        J.set(0, 3, N);
+        J.set(3, 3, Q);
 
         return J.copyArray();
 
+    }
+
+    public static double cos(double x) {
+        return Math.cos(x);
+    }
+
+    public static double sin(double x) {
+        return Math.sin(x);
+    }
+
+    public static double tan(double x) {
+        return Math.tan(x);
+    }
+
+    public static double sec(double x) {
+        return 1/cos(x);
+    }
+
+    public static double sq(double x) {
+        return x*x;
+    }
+
+    public static double sqrt(double x) {
+        return Math.sqrt(x);
     }
 
 }
